17b2d22a319c620e0973fa82fd323d2a87bfa0a2
Isaac <isaac@artstock.io>
2018-02-06 05:24:22 +0900

    Bootstrap ver
    1

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     
-    var songSet = [
+    //이번주 악보
+    let songSet1 = [
        
         //아직까진 여기에 악보사이트 입력하기!!! ㅜㅠ
         "http://cfile25.uf.tistory.com/image/26583B3C56B2F10F018FC5",
@@ -10,16 +11,15 @@ $(document).ready(function() {
         "http://blogfiles.naver.net/20160524_274/lordship46_1464073042426hr2zD_JPEG/%C7%CF%B3%AA%B4%D4%C0%C7_%BC%BC%B0%E8%28%C2%FC_%BE%C6%B8%A7%B4%D9%BF%EE_%B0%F7%C0%CC%B6%F3%29-001-001.jpg",
         
         "http://blogfiles.naver.net/20160524_110/lordship46_146407304340859mkt_JPEG/%C7%CF%B3%AA%B4%D4%C0%C7_%BC%BC%B0%E8%28%C2%FC_%BE%C6%B8%A7%B4%D9%BF%EE_%B0%F7%C0%CC%B6%F3%29-002-002.jpg"
-        
-        
-        
+            
     ]
     
-    for (songs in songSet) {
-        $('.sheet-box').append(`<div class ="row sheet-row"><div class="col-sm-2"></div><div class="col-sm-8"><div class="sheet-placeholder"> <img src="${songSet[songs]}" width="100%"></div></div><div class="col-sm-2"></div></div>`);    
+    for (songs in songSet1) {
+        $('.sheet-box').append(`<div class ="row sheet-row"><div class="col-sm-2"></div><div class="col-sm-8"><div class="sheet-placeholder"> <img src="${songSet1[songs]}" width="100%"></div></div><div class="col-sm-2"></div></div>`);    
     }
+    
+    //저번주 악보
+    //다음주 악보
     
     
 });
-
-var test;
